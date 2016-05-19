08fde5a57b43a9a8916327f8955cc35397e306b1
gcaaa31928 <gcaaa31928@gmail.com>
2016-05-20 00:36:42 +0800

    added settv crawler

--- news/spiders/settv.py
+++ news/spiders/settv.py
@@ -5,24 +5,26 @@ from news.utililties.utility import Utility
 from ..items import NewsItem
 import dateparser
 
-class EasternSpider(scrapy.Spider):
-    name = "eastern"
-    allowed_domains = ["http://star.ettoday.net"]
+class SettvSpider(scrapy.Spider):
+
+
+    name = "settv"
+    allowed_domains = ["http://www.setn.com"]
     start_urls = (
-        "http://star.ettoday.net/news/%d" % int(i) for i in range(697075, 697076)
+        "http://www.setn.com/News.aspx?NewsID=%d" % int(i) for i in range(148002, 148003)
     )
 
     def parse(self, response):
-        if response.css('.error_404').extract():
-            raise CloseSpider('Search Failed')
+        # if response.css('.error_404').extract():
+        #     raise CloseSpider('Search Failed')
         item = NewsItem()
-        item['title'] = response.css('.title::text').extract()[0]
-        content = response.css('.story').extract()[0]
+        item['title'] = response.css('.title h1::text').extract()[0]
+        content = response.css('#Content1').extract()[0]
         item['region'] = Utility.get_location(content)
 
         item['content'] = content
         item['date_time'] = dateparser.parse(response.css('.date::text').extract()[0])
         item['url'] = response.url
-        item['type'] = 'eastern'
+        item['type'] = 'settv'
 
         yield item
