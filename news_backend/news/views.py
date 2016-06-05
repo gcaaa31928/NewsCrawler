22fa857808049b25bb45d9faca0cbf1ef8a402c3
gcaaa31928 <gcaaa31928@gmail.com>
2016-06-05 16:02:59 +0800

    list api added after timestamp

--- news_backend/news/views.py
+++ news_backend/news/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.shortcuts import render
 
 from rest_framework.decorators import api_view
@@ -12,7 +13,12 @@ from news.models import News
 @api_view(['GET'])
 def lists(request):
     limit = request.GET.get('limit', 10)
-    news = News.objects.all()[:int(limit)]
+    print datetime.utcnow()
+    epoch = datetime(1970, 1, 1)
+    now = datetime.utcnow()
+    after_timestamp = int(request.GET.get('after', (now - epoch).total_seconds()))
+    after_datetime = datetime.fromtimestamp(after_timestamp)
+    news = News.objects.filter(date_time__gte=after_datetime)[:int(limit)]
     # print news[0].date_time + '123'
     serializer = NewSerializer(news, many=True)
     return Response(serializer.data)
